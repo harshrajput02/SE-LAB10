c481f7ba7e00948916e8e658e6f022105e859f74
harsh <harsh@10.12.24.27>
2022-04-23 22:30:36 +0530

    SE Lab 10

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,17 @@
+package jUnitTestPackage;
+import java.util.*;
+public class jUnitFunctions 
+{
+	int AddNumbers(int x, int y)
+	{
+		
+		return x+y;
+	}
+	String AddStrings(String a, String b)
+	{
+		String s=a+b;
+		return s;
+	}
+	
+
+}
